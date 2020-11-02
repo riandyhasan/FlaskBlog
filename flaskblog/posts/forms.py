d42207728d378f41cce45a5e4cb78a2d019fc559
Riandy Hasan <16520117@mahasiswa.itb.ac.id>
2020-11-02 21:50:35 +0800

    Flask Blog

--- flaskblog/posts/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired
-
-
-class PostForm(FlaskForm):
-    title = StringField('Judul', validators=[DataRequired()])
-    content = TextAreaField('Isi', validators=[DataRequired()])
-    submit = SubmitField('Post')
